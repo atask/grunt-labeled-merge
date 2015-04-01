19a4bf3dccdaa6cb4a7112fe2caf10a68b1f2476
atask <atask@users.noreply.github.com>
2015-04-01 02:11:52 +0200

    feature(labeled-merge): add MD5 hash file digest

---
+++ tasks/lib/fileHash.js
@@ -0,0 +1,14 @@
+var crypto = require('crypto'),
+    fs = require('fs'),
+    // change the algo to sha1, sha256 etc according to your requirements
+    algo = 'md5';
+
+module.exports = function fileHash(path, callback) {
+    var shasum = crypto.createHash(algo),
+        s = fs.ReadStream(path);
+    s.on('data', function(d) { shasum.update(d); });
+    s.on('end', function() {
+        var d = shasum.digest('hex');
+        return d;
+    });
+}

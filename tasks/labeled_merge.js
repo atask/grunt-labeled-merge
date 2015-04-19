f545653ba00b784f37bd44c725b7440a938bed07
atask <atask@users.noreply.github.com>
2015-04-20 01:50:55 +0200

    fix(labeled-merge): update to use Promises, fix JSHint errors

--- tasks/labeled_merge.js
+++ tasks/labeled_merge.js
@@ -9,61 +9,21 @@
 'use strict';
 
 var mergeMeta = require('./lib/merge-meta'),
-    join = require('path').join,
-    Promise = require('core-js/library/es6/promise'),
-    sep = require('path').sep;
+    Promise = require('core-js/library/es6/promise');
+
 module.exports = function(grunt) {
-
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
     
-  grunt.registerMultiTask('labeled_merge', 'Merges folders without overwriting files.', function(done) {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-            label: function(srcPath) {
-                // Use millisecs as a label
-                return Date.now();
-            }
-        });
-
-    // Iterate over all specified file groups.
-    this.files
-        .filter(function(mapping) {
-            // if not working with folders, forget it
-            var goodSrc = true;
-            mapping.src.forEach(function noDirIn(path) {
-                if (!grunt.file.isDir(path)) {
-                    goodSrc = false;
-                } else if (mergeMeta.hasMeta(path)) {
-                    goodSrc = false;
-                }
+    grunt.registerMultiTask('labeled_merge', 'Merges folders without overwriting files.', function(done) {
+        // Iterate over all specified file groups.
+        this.files.forEach(function(mapping) {
+            var mergePromises = mapping.src.map(function mergeDir(src) {
+                return mergeMeta.merge(mapping.dest, src, this.options);
             });
-            // if source folders ok, init dest if necessary
-            if (goodSrc && !grunt.file.exists(mapping.dest)) {
-                grunt.file.mkdir(mapping.dest);
-            }
-            return goodSrc;
-        })
-        .forEach(function(mapping) {
-            
-            var copyMappings = [],
-                meta = mergeMeta.create({
-                    dir: mapping.dest,
-                    labelFunc: options.label,
-                    copyfunc: function addCopyMapping(srcFile, destFile) {
-                        copyMappings.push(srcFile, destFile);
-                    },
-                    grunt: grunt
-                });
-
-            var fileMergePromises = [];
-            mapping.src.forEach(function mergeDir(srcPath) {
-                grunt.file.recurse(srcPath, function mergeFile() {
-                    fileMergePromises.push(meta.addFile(arguments));
-                }); 
-            });
-            Promise.all(fileMergePromises).then(done);
+            Promise.all(mergePromises)
+                .then(done)
+                .catch(done);
         });
-  });
-
+    });
 };

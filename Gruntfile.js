4946c446c455abac4aba0335eb2e057259bcdb16
atask <atask@users.noreply.github.com>
2015-04-21 01:00:13 +0200

    chore(test-label): reformat with JSHint

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,70 +5,73 @@
  * Copyright (c) 2015 atask
  * Licensed under the MIT license.
  */
-
 'use strict';
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
+        },
 
-    // Copy needed fixture files
-    copy: {
-      fixture: {
-        files: [
-          {expand: true, cwd: 'test/fixture/first/', src: ['**'], dest: 'tmp/default_options_initial/'},
-          {'tmp/default_options_initial/.meta/files.json': 'test/fixtures/metas/first.json'}
-        ]
-      }
-    },
+        // Copy needed fixture files
+        copy: {
+            fixture: {
+                files: [{
+                    expand: true,
+                    cwd: 'test/fixture/first/',
+                    src: ['**'],
+                    dest: 'tmp/default_options_initial/'
+                }, {
+                    'tmp/default_options_initial/.meta/files.json': 'test/fixtures/metas/first.json'
+                }]
+            }
+        },
 
-    // Configuration to be run (and then tested).
-    labeled_merge: {
-      default_options: {
-        files: {
-            'tmp/default_options_initial': ['test/fixtures/first'],
-            'tmp/default_options_first': ['test/fixtures/second']
+        // Configuration to be run (and then tested).
+        labeled_merge: {
+            default_options: {
+                files: {
+                    'tmp/default_options_initial': ['test/fixtures/first'],
+                    'tmp/default_options_first': ['test/fixtures/second']
+                }
+            }
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
         }
-      }
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+    });
 
-  });
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'copy:fixture', 'labeled_merge', 'nodeunit']);
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'copy:fixture', 'labeled_merge', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- test/labeled_merge_test.js
+++ test/labeled_merge_test.js
@@ -27,9 +27,9 @@ var grunt = require('grunt'),
 
 exports.default_options = {
 
-    firstRun: function(test) {
-        var destDir = 'tmp/default_options_initial',
-            filesJson_first = grunt.file.readJSON(
+        firstRun: function(test) {
+            var destDir = 'tmp/default_options_initial',
+                filesJson_first = grunt.file.readJSON(
                     'test/fixtures/metas/first.json'));
         // test meta files
         test.equal(
@@ -61,50 +61,50 @@ exports.default_options = {
     secondRun: function(test) {
         var destDir = 'tmp/default_options_first',
             filesJson_second = grunt.file.readJSON(
-                    'test/fixtures/metas/second.json'));
-        test.deepEqual(
-            grunt.file.readJSON(join(destDir, '.meta/files.json')),
-            filesJson_second,
-            'files.json has wrong content'
-        );
-        // test added and renamed files
-        test.equal(
-            grunt.file.isFile(join(destDir, 'subdir','file7')),
-            true,
-            'subdir/file7 was not created'
-        );
-        test.equal(
-            grunt.file.isFile(join(destDir, 'subdir','file5.c6b0c67e')),
-            true,
-            'subdir/file5.c6b0c67e was not created'
-        );
-        test.equal(
-            grunt.file.isFile(join(destDir, 'subdir','file5.c6b0c67e')),
-            true,
-            'subdir/file5.32db936c was not created'
-        );
-        test.equal(
-            grunt.file.isFile(join(destDir, 'file2.6563acbb.txt')),
-            true,
-            'file2.6563acbb.txt was not created'
-        );
-        test.equal(
-            grunt.file.isFile(join(destDir, 'file2.488fca17.txt')),
-            true,
-            'file2.488fca17.txt was not created'
-        );
-        test.equal(
-            grunt.file.isFile(join(destDir, 'subdir', '.file3.ee5ae220.cfg')),
-            true,
-            'subdir/file3.ee5ae220.cfg was not created'
-        );
-        test.equal(
-            grunt.file.isFile(join(destDir, 'subdir', '.file3.f40a3f71.cfg')),
-            true,
-            'subdir/file3.f40a3f71.cfg was not created'
-        );
-        test.done();
-    }
+                'test/fixtures/metas/second.json'));
+test.deepEqual(
+    grunt.file.readJSON(join(destDir, '.meta/files.json')),
+    filesJson_second,
+    'files.json has wrong content'
+);
+// test added and renamed files
+test.equal(
+    grunt.file.isFile(join(destDir, 'subdir', 'file7')),
+    true,
+    'subdir/file7 was not created'
+);
+test.equal(
+    grunt.file.isFile(join(destDir, 'subdir', 'file5.c6b0c67e')),
+    true,
+    'subdir/file5.c6b0c67e was not created'
+);
+test.equal(
+    grunt.file.isFile(join(destDir, 'subdir', 'file5.c6b0c67e')),
+    true,
+    'subdir/file5.32db936c was not created'
+);
+test.equal(
+    grunt.file.isFile(join(destDir, 'file2.6563acbb.txt')),
+    true,
+    'file2.6563acbb.txt was not created'
+);
+test.equal(
+    grunt.file.isFile(join(destDir, 'file2.488fca17.txt')),
+    true,
+    'file2.488fca17.txt was not created'
+);
+test.equal(
+    grunt.file.isFile(join(destDir, 'subdir', '.file3.ee5ae220.cfg')),
+    true,
+    'subdir/file3.ee5ae220.cfg was not created'
+);
+test.equal(
+    grunt.file.isFile(join(destDir, 'subdir', '.file3.f40a3f71.cfg')),
+    true,
+    'subdir/file3.f40a3f71.cfg was not created'
+);
+test.done();
+}
 };
 
 exports.failureDir = {
@@ -148,11 +148,11 @@ exports.failureDir = {
             'merge-meta did not throw on inexistent source'
         );
     }
-    
+
 };
 
 exports.failureHash = {
-    
+
     setUp: function(callback) {
         grunt.file.copy('test/fixtures/first/file1', 'tmp/targetSource');
         grunt.file.copy('test/fixtures/first/file1', 'tmp/targetDest');
@@ -178,3 +178,4 @@ exports.failureHash = {
         );
     }
 };
+

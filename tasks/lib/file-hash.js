ec6a1c6f2f21580c38bcb2a45a5e4c14cf28a31c
atask <atask@users.noreply.github.com>
2015-04-28 18:28:00 +0200

    fix(labeled-merge): add cross platform hash function checks

--- tasks/lib/file-hash.js
+++ tasks/lib/file-hash.js
@@ -1,4 +1,5 @@
-var crypto = require('crypto'),
+var os = require('os'),
+    crypto = require('crypto'),
     fs = require('fs'),
     Promise = require('core-js/library/es6/promise'),
     // change the algo to sha1, sha256 etc according to your requirements
@@ -7,13 +8,24 @@ var crypto = require('crypto'),
 module.exports = function fileHash(path) {
     'use strict';
     var shasum = crypto.createHash(algo),
-        stream = fs.ReadStream(path);
+        stream = fs.ReadStream(path),
+        readStr;
     return new Promise(function calcHash(resolve, reject) {
-        stream.on('error', reject)
-              .on('data', function(d) { shasum.update(d); })
-              .on('end', function digest() {
-            var d = shasum.digest('hex');
-            resolve(d);
-        });
+        stream
+            .on('error', reject)
+            .on('data', function(data) {
+                readStr = data.toString('utf8');
+                // if Windows, end-of-line encoding will
+                // generate a different MD5 hash.
+                // Better replace it.
+                if (os.EOL === '\r\n') {
+                    readStr = readStr.replace('\r\n', '\n');
+                }
+                shasum.update(readStr);
+            })
+            .on('end', function digest() {
+                var hash = shasum.digest('hex');
+                resolve(hash);
+            });
     });
 };

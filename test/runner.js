56e2e128992e5c55a359878fe0f80bc74325986f
atask <atask@users.noreply.github.com>
2015-05-02 02:51:58 +0200

    test(labeled-merge): adapt runner.js to latest tests

--- test/runner.js
+++ test/runner.js
@@ -1,14 +1,15 @@
 var Promise = require('core-js/library/es6/promise');
 var merger = require('../tasks/lib/merge-meta');
+var fakeHasher = require('./stubs/file-hash.js');
 var EventEmitter = require('events').EventEmitter;
 
-var dest = '../tmp/default_options_initial',
-    src = 'fixtures/second';
-
-merger.merge(dest, src)
+merger.merge('../tmp/runnerDest', '../tmp/runnerSrc1', { getHash: fakeHasher })
     .then( function() {
-        console.log('OK');
-        radium.removeAllListeners('radiation');
+        console.log('OK1');
+        return merger.merge('../tmp/runnerDest', '../tmp/runnerSrc2', { getHash: fakeHasher });
+    })
+    .then (function() {
+        console.log('OK2');
     })
     .catch( function(err) {
         console.error(err);
